5c6cb2c66d353cb85b3d415f40c74cb099c4442d
Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2018-04-30 09:51:09 -0400

    Se anadio un nuevo mensaje

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,7 +5,9 @@
 public class HolaMundo {
   public static void main(String[] args) { 
     // Imprime el mensaje "Hola, Mundo!"
-    System.out.println("Hola, Mundo!"); 
+
+    System.out.println("Hola , Mundo!"); 
+    System.out.println("Bienvenidos a Java!"); 
   }
 } 
 
