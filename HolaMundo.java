54a90aeba20ba4d88af4ff3d1f488bc2c56f316f
Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2018-04-30 09:43:01 -0400

    Se crea el proyecto con primeros archivos

---
+++ HolaMundo.java
@@ -0,0 +1,11 @@
+/*
+ * Ejemplo HolaMundo
+ * Imprime el mensaje "Hola, Mundo!"
+ */
+public class HolaMundo {
+  public static void main(String[] args) { 
+    // Imprime el mensaje "Hola, Mundo!"
+    System.out.println("Hola, Mundo!"); 
+  }
+} 
+
